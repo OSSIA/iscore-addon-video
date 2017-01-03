6043b18630765ac32d3d8a87fb46287a0856ef28
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-01-03 18:35:32 +0100

    refactor

--- iscore_addon_video.hpp
+++ iscore_addon_video.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <iscore/plugins/qt_interfaces/PluginRequirements_QtInterface.hpp>
 #include <iscore/plugins/qt_interfaces/FactoryInterface_QtInterface.hpp>
-#include <iscore/plugins/qt_interfaces/GUIApplicationContextPlugin_QtInterface.hpp>
+#include <iscore/plugins/qt_interfaces/GUIApplicationPlugin_QtInterface.hpp>
 #include <iscore/plugins/qt_interfaces/CommandFactory_QtInterface.hpp>
 #include <QObject>
 #include <utility>
